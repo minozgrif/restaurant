3d30f2b388e3c7372d7afe58d2a57c0a6d76592a
minozgrif <robert.carope@gmail.com>
2025-03-14 17:21:17 -0600

    Avances de PedidoController

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PedidoController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('permission:gestionar_pedidos');
+    }
+
+    public function index()
+    {
+        return Pedido::all();
+    }
 }
